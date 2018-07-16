829ba7c4e4c9c45ea2053987c54f081f9b813714
annastar415z <annastar415z@gmail.com>
2018-07-16 05:14:00 +0300

    6

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use App\Libraries\ControllerHelper;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,8 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
-//        view()->share('config', ControllerHelper::getConfig());
+        Schema::defaultStringLength(191);
     }
 
     /**
